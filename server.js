f2180ca8bc5b05df7802971e0adfb213e91ceab2
vikrant <you@example.com>
2018-03-24 10:27:03 +0530

    modified

--- server.js
+++ server.js
@@ -52,6 +52,14 @@ app.get('/about', (req, res) => {
   });
 })
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'About Page'
+   
+  });
+})
+
+
 
 app.get('/bad', (req, res)=> {
   res.send({
